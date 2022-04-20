3f9b5d02e12c5d7c54a6be93c9891c98fa772331
MirkOrlando <mirko@orlando-m.it>
2022-04-20 14:36:03 +0200

    added image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: '#app',
     data:{
-        message: 'Hello VueJS'
+        message: 'Hello VueJS',
+        imageRnd: '<img src="https://picsum.photos/200/300" alt="imageRnd">'
     }
 })
