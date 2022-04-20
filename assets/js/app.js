aeaff855d3adb146423ac084624820947c6c1828
MirkOrlando <mirko@orlando-m.it>
2022-04-20 14:56:26 +0200

    created second image

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ const app = new Vue({
     el: '#app',
     data:{
         message: 'Hello VueJS',
-        imageRnd: '<img src="https://picsum.photos/200/300" alt="imageRnd">'
+        picsum: 'https://picsum.photos/300/200',
+        imageRnd: '<img src="https://picsum.photos/400/300" alt="imageRnd">'
     }
 })
